013e34d2166afa89749624cc3d49b7b16865af86
EC2 Default User <ec2-user@ip-172-31-4-139.ec2.internal>
2021-12-13 08:45:23 +0000

    [add]エラーメッセージとフラッシュメッセージの追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,34 +1,22 @@
 class UsersController < ApplicationController
 
-
   def top
   end
 
   def new
-    @user = User.new
   end
 
   def index
     @users = User.all
     @book = Book.new
-    @user = User.new
-    @user = @book.user
-  end
-
-
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      redirect_to book_path(@book.id)
-    else
-      render controller: :books, action: :index
-    end
+    @user = current_user
   end
 
   def show
-    @book = Book.new
     @user = User.find(params[:id])
+    @book = Book.new
     @books = @user.books
+
   end
 
 
@@ -49,6 +37,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:title, :body, :profile_image, :introduction, )
+    params.require(:user).permit(:title, :body, :name, :profile_image, :introduction, )
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,10 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   attachment :profile_image
 
-  validates :name, presence: true,
+  validates :name,
     length: { minimum: 3 }
 
-  validates :introduction, presence: true,
+  validates :introduction,
     length: { maximum: 50 }
 
 end
